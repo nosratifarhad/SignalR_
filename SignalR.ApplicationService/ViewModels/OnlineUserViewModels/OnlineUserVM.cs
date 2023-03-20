d2a952fa670c7e56d15c0347743f89fa815c315d
f_nosrati <f_nosrati@SHL-62>
2023-03-20 15:45:41 +0330

    Fix refactor Services

--- SignalR.ApplicationService/ViewModels/OnlineUserViewModels/OnlineUserVM.cs
+++ SignalR.ApplicationService/ViewModels/OnlineUserViewModels/OnlineUserVM.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SignalR.ApplicationService.ViewModels.OnlineUserViewModels
 {
-    public class OnlineUserViewModel
+    public class OnlineUserVM
     {
         public string IpAddress { get; set; }
+
         public string Browser { get; set; }
+        
         public string Country { get; set; }
+        
         public string EntryDate { get; set; }
+        
         public string PresenceDate { get; set; }
+        
         public string OutDate { get; set; }        
+        
         public string OS { get; set; }
+        
         public string Url { get; set; }
 
     }
